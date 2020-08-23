c9f8aabc2ae40f26a82280c37d3d1bcf0ee15c04
Hafeez Ghanchi <hafeezghanchi927@gmail.com>
2020-08-23 21:50:41 +0500

    refactored user component

--- components/UserItem/UserItem.jsx
+++ components/UserItem/UserItem.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useContext, useEffect } from 'react'
-import { StyleSheet, Text, View, Image, FlatList, Modal, TouchableOpacity } from 'react-native'
+import { StyleSheet, Text, View, Image, FlatList, TouchableOpacity } from 'react-native'
 import UserModal from '../UserModal/UserModal'
 import { ContextAPI } from '../store/UserContext'
 
